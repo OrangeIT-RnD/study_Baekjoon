e70b914a573e3caed2304acf00f6904d6b18f18a
th-kwon <th.kwon29@gmail.com>
2021-09-09 09:28:57 +0900

    권태현 - 2021/09/08 풀이내용 추가 및 폴더 정리
    * 풀이한 간단한 문제들을 tutorial로 이동
    * 문제 풀이 추가 - 10818(최대값/최소값), 9498(채점)
       - C#의 경우 각 문제별로 프로젝트 파일이 만들어져야 해서 폴더를 별도로 만들어야 함
    * C#용 스니펫 추가

---
+++ thkwon/tutorial/10818/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10818
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BOJ Problem no.10818
+            // author : thKwon(th.kwon29@gmail.com) - 2021/09/08
+            
+            int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine("debug - input : " + number);
+            string[] inputArray = Console.ReadLine().Split();
+            int max = -1000000, min = 1000000;
+            foreach(string c in inputArray)
+            {
+                int i = int.Parse(c);
+                if(i > max) max = i;
+                if(i < min) min = i;
+                
+                //Console.WriteLine("debug - input : " + i + ", max : " + max + ", min : " + min);
+            }
+            
+            Console.WriteLine(min + " " + max);
+        }
+    }
+}
+
+// note
+// 한번 틀림(정수 최대범위만 확인하고 최소범위를 확인 안함 - -1000000 으로 고침)
---
+++ thkwon/tutorial/9498/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _9498 {
+    class Program {
+        static void Main() {
+            
+            string input = Console.ReadLine();
+            string[] inputArray = input.Split();
+            int number = int.Parse(inputArray[0]);
+
+            if(number >= 90) Console.WriteLine("A");
+            else if(number >= 80) Console.WriteLine("B");
+            else if(number >= 70) Console.WriteLine("C");
+            else if(number >= 60) Console.WriteLine("D");
+            else Console.WriteLine("F");
+        }
+    }
+}
